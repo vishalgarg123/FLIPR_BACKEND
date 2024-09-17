b874bf482595d6aee4d2137c51be6ca38a9c6129
vishal garg <100771603+vishalgarg123@users.noreply.github.com>
2024-09-17 11:35:34 +0530

    INITIAL COMMIT

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -93,12 +93,6 @@ exports.signin_controller = async (req, res) => {
 };
 
 
-const sendTokenResponse = async (user, codeStatus, res) => {
-  const token = await user.getJwtToken();
-  res
-    .status(codeStatus)
-    .cookie("token", token, { maxAge: 60 * 60 * 1000, httpOnly: true })
-    .json({ msg:"SignIn Successfully", token });
-};
+c
 
 
